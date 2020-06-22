87b13269e51a2be00960c436d059176ace44ac1a
Scott Clemons <scott.clemons317@outlook.com>
2020-06-22 13:23:59 -0600

    Add project files.

---
+++ QDS_Review_NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QDS_Review_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
